nextcloud/calendar

migrate to vue 3
Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

src/components/Editor/Properties/PropertyColor.vue
@@ -25,8 +25,8 @@
 			v-else
 			class="property-color__input">
 			<ColorPicker
-				:value="selectedColor"
-				:shown.sync="isColorPickerOpen"
+				v-model:shown="isColorPickerOpen"
+				:model-value="selectedColor"
 				:advanced-fields="true"
 				@submit="changeColor">
 				<NcButton

green-coding-solutions/nextcloud-calendar

src/components/Editor/Properties/PropertyColor.vue
@@ -26,7 +26,7 @@
 			class="property-color__input">
 			<NcColorPicker
 				v-model="selectedColor"
-				:shown.sync="selectorOpen"
+				v-model:shown="selectorOpen"
 				:advanced-fields="true"
 				@update:modelValue="changeColor">
 				<NcButton